cb31916687229e0be757ea76149f4b7ab53bc018
kclay <keyston@conceptual-ideas.com>
2018-09-10 11:58:13 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,12 @@
+const {AkairoClient} = require('discord-akairo');
+require('dotenv').config()
+
+const client = new AkairoClient({
+    ownerID: process.env.DISCORD_OWNER_ID.split(','), // or ['123992700587343872', '86890631690977280']
+    prefix: (process.env.DISCORD_PREFIX || '!').split(','), // or ['?', '!']
+    commandDirectory: './commands/',
+    commandUtil: true,
+    handleEdits: true
+});
+
+client.login(process.env.DISCORD_TOKEN);
